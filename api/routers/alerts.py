eb6ff57bc3956e71e05a59971982a54b887a034f
Pat Heard <patrick.heard@cds-snc.ca>
2023-02-11 18:15:43 +0000

    fix: revert to raw body await

--- api/routers/alerts.py
+++ api/routers/alerts.py
@@ -1,7 +1,7 @@
 """
 API routes for the GitHub alerts requests
 """
-from fastapi import APIRouter, Body, Request, Response, status
+from fastapi import APIRouter, Request, Response, status
 from utils.crypto import is_valid_signature
 from utils.logger import logger
 
@@ -9,11 +9,14 @@ router = APIRouter()
 
 
 @router.post("/alert", status_code=status.HTTP_201_CREATED)
-async def receive_alert(request: Request, response: Response, payload: str = Body(...)):
+async def receive_alert(request: Request, response: Response):
     "Request from GitHub that a secret has been detected"
 
     public_key_id = request.headers.get("GITHUB-PUBLIC-KEY-IDENTIFIER")
     public_key_signature = request.headers.get("GITHUB-PUBLIC-KEY-SIGNATURE")
+
+    raw_payload = await request.body()
+    payload = raw_payload.decode("utf-8")
 
     if is_valid_signature(public_key_id, public_key_signature, payload):
         logger.info("%s", payload)
