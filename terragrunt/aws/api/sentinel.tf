b0311c97396fa877bcf792aacbe528a914e5a34a
Pat Heard <patrick.heard@cds-snc.ca>
2023-02-13 16:47:19 -0500

    feat: send API logs to Sentinel (#16)
    Add the Sentinel Forwarder module to send the API's
    logs to Sentinel for analysis.  Detected secrets are logged
    with a consistent string pattern as JSON so these can
    be alerted on.

---
+++ terragrunt/aws/api/sentinel.tf
@@ -0,0 +1,14 @@
+module "sentinel_forwarder" {
+  source            = "github.com/cds-snc/terraform-modules?ref=v5.0.1//sentinel_forwarder"
+  function_name     = var.product_name
+  billing_tag_value = var.billing_code
+
+  layer_arn = "arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:42"
+
+  customer_id = var.log_analytics_workspace_id
+  shared_key  = var.log_analytics_workspace_key
+
+  cloudwatch_log_arns = [
+    "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/lambda/${module.api.function_name}"
+  ]
+}
