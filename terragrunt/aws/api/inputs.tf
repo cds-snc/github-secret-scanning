0651edce88fb9ff6be0d9961a8125c4b00cb4f30
Pat Heard <patrick.heard@cds-snc.ca>
2023-02-13 13:22:41 -0500

    fix: update to use an API `.env` (#11)
    Add a `github-secret-scanning-config` that writes a `.env` on Lambda
    startup.  We can use this to set all the API's private env vars.

--- terragrunt/aws/api/inputs.tf
+++ terragrunt/aws/api/inputs.tf
@@ -1,5 +1,5 @@
-variable "github_token" {
-  description = "The GitHub token used to retrieve the public key used to verify the alert signature."
+variable "api_config" {
+  description = "The API environment variables that are written to a .env read by the Lambda function."
   type        = string
   sensitive   = true
 }
