697d952a6a02fc60bd300f5a60810a412271fa70
Pat Heard <patrick.heard@cds-snc.ca>
2023-04-17 15:19:17 -0400

    fix: add Sentinel forward subscription
    Add a Sentinel forwarder subscription that triggers when
    a secret is detected in the logs.

--- terragrunt/aws/api/sentinel.tf
+++ terragrunt/aws/api/sentinel.tf
@@ -1,14 +1,27 @@
+locals {
+  api_log_group_name           = "/aws/lambda/${module.api.function_name}"
+  api_log_group_arn            = "arn:aws:logs:${var.region}:${var.account_id}:log-group:${local.api_log_group_name}"
+}
+
 module "sentinel_forwarder" {
-  source            = "github.com/cds-snc/terraform-modules?ref=v5.0.1//sentinel_forwarder"
+  source            = "github.com/cds-snc/terraform-modules?ref=v5.1.5//sentinel_forwarder"
   function_name     = "${var.product_name}-sentinel"
   billing_tag_value = var.billing_code
 
-  layer_arn = "arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:42"
+  layer_arn = "arn:aws:lambda:ca-central-1:283582579564:layer:aws-sentinel-connector-layer:54"
 
   customer_id = var.log_analytics_workspace_id
   shared_key  = var.log_analytics_workspace_key
 
   cloudwatch_log_arns = [
-    "arn:aws:logs:${var.region}:${var.account_id}:log-group:/aws/lambda/${module.api.function_name}"
+    local.api_log_group_arn
   ]
+}
+
+resource "aws_cloudwatch_log_subscription_filter" "secret_detected" {
+  name            = "Secret detected"
+  log_group_name  = local.api_log_group_name
+  filter_pattern  = "Secret detected"
+  destination_arn = module.sentinel_forwarder.lambda_arn
+  distribution    = "Random"
 }
