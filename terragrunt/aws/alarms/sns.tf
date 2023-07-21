3df6d6acd63a8257f49e2ec23082607934c19d0f
Pat Heard <patrick.heard@cds-snc.ca>
2023-07-21 15:57:49 -0400

    feat: add SNS topic for CloudWatch alarms
    Add a dedicated SNS topic for CloudWatch alarms.
    This will allow us to customize the Slack webhook used to
    deliver alarm notifications.

---
+++ terragrunt/aws/alarms/sns.tf
@@ -0,0 +1,62 @@
+#
+# SNS: topics
+#
+resource "aws_sns_topic" "cloudwatch_alarm" {
+  name              = "github-secret-scanning-cloudwatch-alarm"
+  kms_master_key_id = aws_kms_key.sns_cloudwatch.id
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+  }
+}
+
+#
+# SNS: subscriptions
+#
+resource "aws_sns_topic_subscription" "cloudwatch_alarm" {
+  topic_arn = aws_sns_topic.cloudwatch_alarm.arn
+  protocol  = "https"
+  endpoint  = var.slack_webhook_url
+}
+
+#
+# KMS: SNS topic encryption keys
+#
+resource "aws_kms_key" "sns_cloudwatch" {
+  # checkov:skip=CKV_AWS_7: key rotation not required for CloudWatch SNS topic's messages
+  description = "KMS key for CloudWatch alarm SNS topic"
+  policy      = data.aws_iam_policy_document.sns_cloudwatch.json
+
+  tags = {
+    (var.billing_tag_key) = var.billing_tag_value
+  }
+}
+
+data "aws_iam_policy_document" "sns_cloudwatch" {
+  # checkov:skip=CKV_AWS_109: `resources = ["*"]` identifies the KMS key to which the key policy is attached
+  # checkov:skip=CKV_AWS_111: `resources = ["*"]` identifies the KMS key to which the key policy is attached
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions   = ["kms:*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${var.account_id}:root"]
+    }
+  }
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+    actions = [
+      "kms:Decrypt",
+      "kms:GenerateDataKey*",
+    ]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudwatch.amazonaws.com"]
+    }
+  }
+}
