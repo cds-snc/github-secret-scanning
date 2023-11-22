a6ac65a96313c3167a1c7ece2bf399101f001448
Sylvia McLaughlin <sylvia.mclaughlin@cds-snc.ca>
2023-11-22 15:32:03 -0800

    Adding more descriptions and converting all tabs to spaces

--- terragrunt/aws/alert_compromise/functions/broadcast_alert.py
+++ terragrunt/aws/alert_compromise/functions/broadcast_alert.py
@@ -22,8 +22,12 @@ def lambda_handler(event, context):
     decoded_event = json.loads(gzip.decompress(
         base64.b64decode(event['awslogs']['data'])))
     message = decoded_event['logEvents'][0]['message']
+
+
+    print("Starting...")
     # Double check that the message received contains a secret
     if ("Secret detected:" in message and "dsp-testing" not in message):
+        print("Secret has been tetected!")
         message_array = re.split("\s", message)
         for element in message_array:
             # Retrieve the api_key and the github_repo from the message
@@ -37,8 +41,10 @@ def lambda_handler(event, context):
                 source = element
         body = f"API Key with value {token}, {type} and {source} has been detected in {github_repo}!"
         # Publish the alert to the SNS topic
+        print("Publishing to SNS topic")
         boto3.client('sns').publish(
             TargetArn=os.environ['sns_topic_arn'],
             Message=body,
             Subject=os.environ['subject']
         )
+        print("Done...")
