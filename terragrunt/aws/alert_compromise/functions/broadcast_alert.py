04b5a092aedbc44c1cf8ecd885759218ac5f36ec
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2023-11-24 14:40:38 -0800

    Fixing inconsistent spacing and tabbing in lambda function + Add logging (#166)
    * Running function through autopep8 to fix issues
    
    * Adding more descriptions and converting all tabs to spaces

--- terragrunt/aws/alert_compromise/functions/broadcast_alert.py
+++ terragrunt/aws/alert_compromise/functions/broadcast_alert.py
@@ -5,7 +5,8 @@ import base64
 import os
 import re
 
-#===============================================================================
+# ===============================================================================
+
 
 def lambda_handler(event, context):
     """
@@ -18,25 +19,32 @@ def lambda_handler(event, context):
     Returns:
         None
     """
-    decoded_event = json.loads(gzip.decompress(base64.b64decode(event['awslogs']['data'])))
+    decoded_event = json.loads(gzip.decompress(
+        base64.b64decode(event['awslogs']['data'])))
     message = decoded_event['logEvents'][0]['message']
+
+
+    print("Starting...")
     # Double check that the message received contains a secret
     if ("Secret detected:" in message and "dsp-testing" not in message):
+        print("Secret has been tetected!")
         message_array = re.split("\s", message)
         for element in message_array:
             # Retrieve the api_key and the github_repo from the message
-            if "token=" in element: 
+            if "token=" in element:
                 token = element
             elif "url=" in element:
                 github_repo = element
-	    elif "type=" in element:
-		type = element
-	    elif "source=" in element:
-		source = element 
+            elif "type=" in element:
+                type = element
+            elif "source=" in element:
+                source = element
         body = f"API Key with value {token}, {type} and {source} has been detected in {github_repo}!"
-        # Publish the alert to the SNS topic 
+        # Publish the alert to the SNS topic
+        print("Publishing to SNS topic")
         boto3.client('sns').publish(
-            TargetArn = os.environ['sns_topic_arn'],
-            Message = body,
-            Subject = os.environ['subject']
-            )
+            TargetArn=os.environ['sns_topic_arn'],
+            Message=body,
+            Subject=os.environ['subject']
+        )
+        print("Done...")
