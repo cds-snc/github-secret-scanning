136663f771b2f850436cf567e695650d2bacad7a
Pat Heard <patrick.heard@cds-snc.ca>
2024-02-07 11:45:20 -0500

    chore: ignore more GitHub tests requests (#208)
    Update the CloudWatch alarm and alert compromise to ignore more
    GitHub test request patterns.
    
    Add unit tests, linting and formatting for the alert
    compromise function.

--- terragrunt/aws/alarms/locals.tf
+++ terragrunt/aws/alarms/locals.tf
@@ -3,4 +3,14 @@ locals {
   error_logged_api         = "ErrorLoggedAPI"
   metric_namespace         = "GitHubSecretScanning"
   secret_detected_api      = "SecretDetectedAPI"
+
+  secret_detected_metric = [
+    "Secret detected",
+  ]
+  secret_detected_metric_skip = [
+    "dsp-testing",
+    "example.com",
+    "gcntfy-github-test-revoked",
+  ]
+  secret_detected_metric_pattern = "[(w1=\"*${join("*\" || w1=\"*", local.secret_detected_metric)}*\") && w1!=\"*${join("*\" && w1!=\"*", local.secret_detected_metric_skip)}*\"]"
 }
