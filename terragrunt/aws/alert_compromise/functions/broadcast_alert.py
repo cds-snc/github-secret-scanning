ae1d881a61755a7858a811a66d8dd7eea971a80f
Sylvia McLaughlin <85905333+sylviamclaughlin@users.noreply.github.com>
2024-03-26 11:02:41 -0700

    Fixing the lambda function to handle multiple entries (#247)

--- terragrunt/aws/alert_compromise/functions/broadcast_alert.py
+++ terragrunt/aws/alert_compromise/functions/broadcast_alert.py
@@ -23,7 +23,6 @@ def lambda_handler(event, context):
     decoded_event = json.loads(
         gzip.decompress(base64.b64decode(event["awslogs"]["data"]))
     )
-    message = decoded_event["logEvents"][0]["message"]
 
     # List of items to ignore as these are from GitHub testing
     ignore_terms = [
@@ -40,28 +39,33 @@ def lambda_handler(event, context):
     ignore_terms.append(notify_doc_api_key)
 
     print("Starting...")
-    # Double check that the message received contains a secret
-    if "Secret detected:" in message and not any(
-        term in message for term in ignore_terms
-    ):
-        print("Secret has been detected!")
-        message_array = re.split(r"\s", message)
-        for element in message_array:
-            # Retrieve the api_key and the github_repo from the message
-            if "token=" in element:
-                token = element
-            elif "url=" in element:
-                github_repo = element
-            elif "type=" in element:
-                type = element
-            elif "source=" in element:
-                source = element
-        body = f"API Key with value {token}, {type} and {source} has been detected in {github_repo}!"  # noqa: E501
-        # Publish the alert to the SNS topic
-        print("Publishing to SNS topic")
-        boto3.client("sns").publish(
-            TargetArn=os.environ["sns_topic_arn"],
-            Message=body,
-            Subject=os.environ["subject"],
-        )
-        print("Done...")
+
+    for log_event in decoded_event["logEvents"]:
+        # get the message from the logs
+        message = log_event["message"]
+        # Double check that the message received contains a secret
+        if "Secret detected:" in message and not any(
+            term in message for term in ignore_terms
+        ):
+            print("Secret has been detected!")
+            message_array = re.split(r"\s", message)
+            for element in message_array:
+                # Retrieve the api_key and the github_repo from the message
+                if "token=" in element:
+                    token = element
+                elif "url=" in element:
+                    github_repo = element
+                elif "type=" in element:
+                    type = element
+                elif "source=" in element:
+                    source = element
+            body = f"API Key with value {token}, {type} and {source} has been detected in {github_repo}!"  # noqa: E501
+            # Publish the alert to the SNS topic
+            print("Publishing to SNS topic")
+            boto3.client("sns").publish(
+                TargetArn=os.environ["sns_topic_arn"],
+                Message=body,
+                Subject=os.environ["subject"],
+            )
+            print("Alert published for one event...")
+    print("Done processing all events...")
